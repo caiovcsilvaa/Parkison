0b4c92219ee6d86cc89d045f744bd6407c13e62e
Caio Victor <caiovictorsilva07@gmail.com>
2024-08-29 18:27:24 -0300

    Criação da legenda dos eixos.

--- ParkinsonData/Interface/interfacefluidaSeparadas.py
+++ ParkinsonData/Interface/interfacefluidaSeparadas.py
@@ -14,37 +14,37 @@ class ArduinoApp:
         self.root = root
         self.root.title("Interface de Coleta de Dados do Arduino")
 
-        #Variáveis de controle
+        # Variáveis de controle
         self.gravando = False
         self.status_conexao = tk.StringVar(value="Desconectado")
 
-        #Setup da porta serial
+        # Setup da porta serial
         self.porta_serial = 'COM5'
         self.baud_rate = 115200
         self.timeout = 0.1
 
-        #Configuração inicial do Matplotlib
+        # Configuração inicial do Matplotlib
         sns.set(style="whitegrid")
         self.fig, (self.ax1, self.ax2, self.ax3) = plt.subplots(3, 1, figsize=(8, 8))
         plt.subplots_adjust(hspace=0.5)  # Espaçamento entre os gráficos
 
-        #Arrays para armazenar os dados
+        # Arrays para armazenar os dados
         self.leitura1 = []
         self.leitura2 = []
         self.leitura3 = []
 
-        #Arrays para armazenar os dados interpolados
+        # Arrays para armazenar os dados interpolados
         self.interp_leitura1 = []
         self.interp_leitura2 = []
         self.interp_leitura3 = []
 
-        #Configuração da interface gráfica
+        # Configuração da interface gráfica
         self.configurar_interface()
 
-        #Configuração da conexão serial
+        # Configuração da conexão serial
         self.configurar_conexao_serial()
 
-        #Configuração da animação
+        # Configuração da animação
         self.anim = FuncAnimation(self.fig, self.atualizar_grafico, interval=200, cache_frame_data=False)
 
     def configurar_interface(self):
@@ -63,7 +63,7 @@ class ArduinoApp:
         status_value = ttk.Label(frame, textvariable=self.status_conexao)
         status_value.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
 
-        #Embedding Matplotlib figure into Tkinter
+        # Embedding Matplotlib figure into Tkinter
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.canvas.get_tk_widget().grid(row=2, column=0, columnspan=2)
 
@@ -88,7 +88,7 @@ class ArduinoApp:
         self.ax2.clear()
         self.ax3.clear()
 
-        #Atualizar os gráficos com os dados interpolados
+        # Atualizar os gráficos com os dados interpolados
         self.ax1.plot(self.interp_leitura1, label='Leitura 1', color='r')
         self.ax2.plot(self.interp_leitura2, label='Leitura 2', color='g')
         self.ax3.plot(self.interp_leitura3, label='Leitura 3', color='b')
@@ -96,6 +96,14 @@ class ArduinoApp:
         self.ax1.set_ylim(bottom=0, top=10000)
         self.ax2.set_ylim(bottom=0, top=10000)
         self.ax3.set_ylim(bottom=0, top=10000)
+
+        # Adicionando rótulos aos eixos
+        self.ax1.set_xlabel("Dados coletados a cada 100 amostras interpoladas")
+        self.ax1.set_ylabel("Pressão em Pascal (Pa)")
+        self.ax2.set_xlabel("Dados coletados a cada 100 amostras interpoladas")
+        self.ax2.set_ylabel("Pressão em Pascal (Pa)")
+        self.ax3.set_xlabel("Dados coletados a cada 100 amostras interpoladas")
+        self.ax3.set_ylabel("Pressão em Pascal (Pa)")
 
         self.ax1.legend(loc='upper right')
         self.ax2.legend(loc='upper right')
@@ -108,13 +116,10 @@ class ArduinoApp:
         if self.leitura3:
             self.ax3.text(0.02, 0.95, f'Leitura 3: {self.leitura3[-1]:.2f}', transform=self.ax3.transAxes, verticalalignment='top')
 
-
-
     def iniciar_gravacao(self):
         self.gravando = True
         self.status_conexao.set("Conectado")
         threading.Thread(target=self.receber_dados).start()  # Iniciar uma thread separada para a leitura serial
-
 
     def parar_gravacao(self):
         self.gravando = False
@@ -136,7 +141,7 @@ class ArduinoApp:
                             self.leitura2.pop(0)
                             self.leitura3.pop(0)
 
-                        #Interpolação linear para suavizar as transições entre os dados
+                        # Interpolação linear para suavizar as transições entre os dados
                         self.interp_leitura1 = np.interp(np.linspace(0, len(self.leitura1) - 1, 500), np.arange(len(self.leitura1)), self.leitura1)
                         self.interp_leitura2 = np.interp(np.linspace(0, len(self.leitura2) - 1, 500), np.arange(len(self.leitura2)), self.leitura2)
                         self.interp_leitura3 = np.interp(np.linspace(0, len(self.leitura3) - 1, 500), np.arange(len(self.leitura3)), self.leitura3)
@@ -151,7 +156,7 @@ class ArduinoApp:
         self.ser.close()
         self.root.destroy()
 
-#Inicializa a interface
+# Inicializa a interface
 if __name__ == "__main__":
     root = tk.Tk()
     app = ArduinoApp(root)
